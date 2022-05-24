35ff0b77a6f8aab94569564d445c73d838d2bef6
ArmZaDev <nopparat.p62@rsu.ac.th>
2022-05-24 12:19:49 +0700

    add file beginning.cs

---
+++ beginning.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_World
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //การแสดงผล(Wirte/WriteLine)
+            Console.Write("KesukeTakoyaki");
+            Console.WriteLine("SobaNoodle");
+            Console.ReadKey();
+
+            //กำหนดลำดับการแสดงผลด้วยปีกกา
+            Console.WriteLine("FirstName: {0} LastName: {1}", "Nobpharat", "Pongdong");               
+            Console.WriteLine("{0} + {2} = {1}, "50", "80", "30");
+            Console.ReadKey(); 
+                           
+            //Variable 
+            int myNum = 26;
+            double myDoubleNum = 10.99D;
+            char myChar = 'A';
+            bool myBool = true;
+            string myString = "Hello Wold";
+            const int MyNum = 10;
+
+            Console.WriteLine("int = " + myNum);
+            Console.WriteLine("double = " + myDoubleNum);
+            Console.WriteLine("char = " + myChar);
+            Console.WriteLine("bool = " + myBool);
+            Console.WriteLine("string = " + myString);
+            Console.WriteLine("const = " + MyNum);
+            Console.ReadKey();
+            
+            //format string
+            int number = 120000;
+            Console.WriteLine("Number = {0}", number);
+            Console.WriteLine("Number = {0:E}", number); //Exponential
+            Console.WriteLine("Number = {0:F4}", number); //Floating Point
+            Console.WriteLine("Number = {0:G}", number); //เเสดงตัวเลขรูปเเบสั้นที่สุด
+            Console.WriteLine("Number = {0:N}", number); //comma
+            Console.WriteLine("Number = {0:P}", number); //%
+            Console.WriteLine("Number = {0:X}", number); //Hex
+            Console.ReadKey();
+           
+            //รับค่าผ่านทางคีย์บอร์ด
+            Console.Write("UserName: ");
+            string username = Console.ReadLine();
+            Console.Write("Password: ");            
+            string password = Console.ReadLine();
+            Console.WriteLine("UserName is: " + username + "\n" + "Password is: " + password);
+            Console.Write("Aa-Zz to ASCII Code: ");
+            int Ascii = Console.Read();
+            Console.WriteLine("Ascii = " + Ascii);
+
+
+        }
+    }
+}
